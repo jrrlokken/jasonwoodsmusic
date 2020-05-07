00225693af2b2e5dc492e2427d7fca89aaee3549
Joshua Lokken <jrrlokken@gmail.com>
2020-05-07 08:48:29 -0500

    Fixed a typo in app.js, lengthened transition time on Work page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,31 @@
 // document.addEventListener('DOMContentLoaded', function() {
-const typedHeading = new Typed(".jumbotron-heading", {
-  strings: ["Engineer.  Producer.  Musician."],
+const typedHeading = new Typed('.jumbotron-heading', {
+  strings: ['Engineer.  Producer.  Musician.'],
   typeSpeed: 100,
   startDelay: 1000,
   loop: false,
 });
 
-const $bookingModal = $("#bookingModal");
-const $bookingForm = $("#booking-form");
-const $workAlbum = $(".album");
-const $workButton = $("#work-button");
+const $bookingModal = $('#bookingModal');
+const $bookingForm = $('#booking-form');
+const $workAlbum = $('.album');
+const $workButton = $('#work-button');
 
 $workAlbum.hide();
 
 function getWorkBtnText() {
-  let isClicked = $workButton.hasClass("clicked");
-  let btnText = isClicked ? "Back" : "Work";
+  let isClicked = $workButton.hasClass('clicked');
+  let btnText = isClicked ? 'Back' : 'Work';
   return btnText;
 }
 
 $workButton.text(getWorkBtnText());
 
-$workButton.on("click", function () {
-  $workButton.toggleClass("clicked");
+$workButton.on('click', function () {
+  $workButton.toggleClass('clicked');
   $workButton.text(getWorkBtnText());
-  $("#landing-page").toggleClass("work");
-  if (getWorkBtnText() === "Work") {
+  $('#landing-page').toggleClass('work');
+  if (getWorkBtnText() === 'Work') {
     $workAlbum.hide();
   } else {
     $workAlbum.show();
@@ -35,18 +35,18 @@ $workButton.on("click", function () {
 $bookingForm.submit(function (event) {
   event.preventDefault();
 
-  const $firstName = $("#firstName").val();
-  const $lastName = $("#lastName").val();
-  const $email = $("#email").val();
-  const $startDate = $("#startDate").val();
-  const $endDate = $("#endDate").val();
-  const $proposalText = $("#proposalText").val();
+  const $firstName = $('#firstName').val();
+  const $lastName = $('#lastName').val();
+  const $email = $('#email').val();
+  const $startDate = $('#startDate').val();
+  const $endDate = $('#endDate').val();
+  const $proposalText = $('#proposalText').val();
 
-  const subject = "New Session Booking";
-  const to_email = "jrrlokken@gmail.com";
+  const subject = 'New Session Booking';
+  const to_email = 'jrrlokken@gmail.com';
   const body = `Booking proposal from ${$firstName} ${$lastName}:
      Proposed booking dates: ${$startDate} - ${$endDate}
-     Email address: ${email}
+     Email address: ${$email}
 
      ${$proposalText}
     `;
@@ -54,8 +54,8 @@ $bookingForm.submit(function (event) {
   window.location.href = `mailto:${to_email}?subject=${subject}&body=${body}`;
   // console.log($firstName, $lastName, $email);
 
-  $bookingModal.modal("hide");
-  $bookingForm.trigger("reset");
+  $bookingModal.modal('hide');
+  $bookingForm.trigger('reset');
 });
 
 // });
